6595b0b5c0f0276f1d4edc5b5b35d091658a08df
Henry <hkastler+github@gmail.com>
2017-10-20 10:13:03 -0500

    serving files from the jar

--- src/main/java/com/hkstlr/apr2aw2/App.java
+++ src/main/java/com/hkstlr/apr2aw2/App.java
@@ -1,9 +1,10 @@
 package com.hkstlr.apr2aw2;
 
-import static io.undertow.Handlers.resource;
+import io.undertow.Handlers;
+import static io.undertow.Handlers.path;
 import io.undertow.Undertow;
-import io.undertow.server.handlers.resource.FileResourceManager;
-import java.io.File;
+import io.undertow.server.handlers.resource.ClassPathResourceManager;
+import io.undertow.server.handlers.resource.ResourceHandler;
 
 /**
  * Hello world!
@@ -13,15 +14,22 @@ public class App {
 
     public static void main(final String[] args) {
         try {
+            
+            ClassPathResourceManager resourceManager = new ClassPathResourceManager(
+                    App.class.getClassLoader(),"apr2aw");
+            ResourceHandler resourceHandler = Handlers.resource(resourceManager)
+                    .setResourceManager(resourceManager)
+                    .setWelcomeFiles("index.html")
+                    .setDirectoryListingEnabled(false);
+            
             Undertow server = Undertow.builder()
                     .addHttpListener(8080, "localhost")
-                    .setHandler(
-                            resource(new FileResourceManager(new File("src/main/webapp"), 1024))
-                            .setDirectoryListingEnabled(false)
-                            .setWelcomeFiles("index.html","/apr2aw/index.html")
-                            )
+                    .setHandler(path()
+                        .addPrefixPath("/",resourceHandler)
+                        .addPrefixPath("/apr2aw/", resourceHandler)
+                    )
                     .build();
-                    
+
             server.start();
         } catch (Exception e) {
             // TODO Auto-generated catch block
